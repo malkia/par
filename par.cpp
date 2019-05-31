b8830018c92bae533e5db490d7e82ac15df4b1e1
Dimiter 'malkia' Stanev <malkia@gmail.com>
2019-05-30 23:37:08 -0700

    changes...

--- par.cpp
+++ par.cpp
@@ -3,37 +3,47 @@
 #include <chrono>
 #include <stdio.h>
 
-volatile std::byte x = std::byte(0x3a);
+volatile size_t start = 0;
+volatile size_t end = 16;
 
 int main()
 {
-    std::vector<std::byte> data;
-    size_t total = 1024ull*1024ull*1024;
+    std::vector<uint8_t> data;
+    size_t total = 1024ull*1024ull*128;
     data.reserve(total);
-    for( auto bytes=16384; bytes<total; bytes = bytes * 2 )
+    for( auto bytes=4096; bytes<total; bytes = bytes * 2 )
     {
         size_t cycles = total / bytes;
         data.resize(bytes);
         auto t1 = std::chrono::steady_clock::now();
-        for( auto i=0; i<cycles; i++ )
+        for( volatile auto v=start; v<end; v++ )
+        for( volatile auto i=size_t(0); i<cycles; i++ )
         {
-            x = std::byte(int(x)+1);
-            std::for_each( std::execution::seq, data.begin(), data.end(), [&]( std::byte& b ){
-                b = std::byte(int(b) ^ int(x));
+            std::for_each( std::execution::seq, data.begin(), data.end(), [&]( auto& b ){
+                b = int(v) * int(i) + 1;
             });    
         }
         auto t2 = std::chrono::steady_clock::now();
-        for( auto i=0; i<cycles; i++ )
+        for( volatile auto v=start; v<end; v++ )
+        for( volatile auto i=size_t(0); i<cycles; i++ )
         {
-            x = std::byte(int(x)+1);
-            std::for_each( std::execution::par_unseq, data.begin(), data.end(), [&]( std::byte& b ){
-                b = std::byte(int(b) ^ int(x));
+            std::for_each( std::execution::par, data.begin(), data.end(), [&]( auto& b ){
+                b = int(v) * int(i) + 1;
             });    
         }
         auto t3 = std::chrono::steady_clock::now();
+        for( volatile auto v=start; v<end; v++ )
+        for( volatile auto i=size_t(0); i<cycles; i++ )
+        {
+            std::for_each( std::execution::par_unseq, data.begin(), data.end(), [&]( auto& b ){
+                b = int(v) * int(i) + 1;
+            });    
+        }
+        auto t4 = std::chrono::steady_clock::now();
         auto d1 = (t2 - t1).count();
         auto d2 = (t3 - t2).count();
-        printf( "b=%d %5.3f %5.3f secs\n", bytes, (double)d1 / 1e9, double(d2)/1e9 );
+        auto d3 = (t4 - t3).count();
+        printf( "b=%d %5.3f %5.3f %5.3f secs\n", bytes, (double)d1 / 1e9, double(d2)/1e9, double(d3)/1e9 );
     }
     return 0;
 }
